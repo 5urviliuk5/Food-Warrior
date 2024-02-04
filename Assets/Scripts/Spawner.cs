c3d577680f39e56826aeacc95e8d6b83aca71c93
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-02-04 16:59:47 +0200

    bomb added

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,25 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FruitSpawner : MonoBehaviour
 {
-    public GameObject fruitPrefab;
-    public float spawnRadius = 2f;
+    public GameObject[] fruits; // Array of fruit prefabs
 
-    private float nextSpawnTime = 0f;
-
-    void Update()
+    void Start()
     {
-        if (Time.time >= nextSpawnTime)
-        {
-            SpawnFruit();
-            nextSpawnTime = Time.time;
-        }
+        InvokeRepeating("Spawn", 1f, 1f);
     }
 
-    void SpawnFruit()
+    void Spawn()
     {
-        Vector3 randomSpawnPos = transform.position + Random.insideUnitSphere * spawnRadius;
-        randomSpawnPos.y = -1f;
-        Instantiate(fruitPrefab, randomSpawnPos, Quaternion.identity);
+        int numberOfFruits = Random.Range(1, 6); // Get a random number between 1 and 5
+
+        for (int i = 0; i < numberOfFruits; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-5, 5), -5);
+            int randomIndex = Random.Range(0, fruits.Length); // Get a random index between 0 and fruits.Length-1
+            Instantiate(fruits[randomIndex], pos, Quaternion.identity); // Spawn the fruit at the random position
+        }
     }
 }
