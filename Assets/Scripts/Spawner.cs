e18a068d5c814dea2a7a5da31942ecf83a49be38
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-02-08 18:27:53 +0200

    spwner script

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,25 +1,20 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FruitSpawner : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
-    public GameObject[] fruits; // Array of fruit prefabs
+    public List<GameObject> fruits;
 
     void Start()
     {
-        InvokeRepeating("Spawn", 1f, 1f);
+        InvokeRepeating(nameof(Spawn), 1f, 1f);
     }
 
     void Spawn()
     {
-        int numberOfFruits = Random.Range(1, 6); // Get a random number between 1 and 5
-
-        for (int i = 0; i < numberOfFruits; i++)
-        {
-            Vector3 pos = new Vector3(Random.Range(-5, 5), -5);
-            int randomIndex = Random.Range(0, fruits.Length); // Get a random index between 0 and fruits.Length-1
-            Instantiate(fruits[randomIndex], pos, Quaternion.identity); // Spawn the fruit at the random position
-        }
+        var fruit = fruits[Random.Range(0, fruits.Count)];
+        var obj = Instantiate(fruit);
+        var x = Random.Range(-5f, 5f);
+        obj.transform.position = new Vector3(x, -4, 0);
     }
 }
